79e6584525a9b185177d8cecd983647d811a1d6f
phacUFPE <phac@cin.ufpe.br>
2018-11-05 13:21:50 -0300

    Primeiro Commit

---
+++ Hasher/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Hasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker_DoWork);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(510, 46);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Iniciar Hasher";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 70);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Hasher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Hasher/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hasher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string CalculateMD5(string filename)
+        {
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(filename))
+                    {
+                        var hash = md5.ComputeHash(stream);
+                        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("ERRO: {0}", ex.Message));
+                return "NONE";
+            }
+        }
+
+        private void RecursiveHash(string directory, StreamWriter fileToWrite)
+        {
+            string more = directory.Remove(0, AppDomain.CurrentDomain.BaseDirectory.Length);
+            try
+            {
+                if (Directory.Exists(directory))
+                {                    
+                    string[] files = Directory.GetFiles(directory);
+                    foreach (string hFile in files)
+                    {
+                        if (hFile.Contains("_hlist") || hFile.Contains("Hasher.exe")) { continue; }
+                        string hash = CalculateMD5(hFile);
+                        fileToWrite.WriteLine(String.Format("file=\"{0}\", hash=\"{1}\"", more + hFile.Replace(directory, ""), hash));
+                    }
+                    string[] dirs = Directory.GetDirectories(directory);
+
+                    foreach (string d in dirs)
+                    {
+                        RecursiveHash(d, fileToWrite);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("ERRO: {0}", ex.Message));
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {                        
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "_hlist"))            
+            {
+                File.Create(AppDomain.CurrentDomain.BaseDirectory + "_hlist").Close();                
+            }
+            try
+            {
+                StreamWriter file = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "_hlist", false);
+                RecursiveHash(AppDomain.CurrentDomain.BaseDirectory, file);
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("ERRO: {0}", ex.Message));
+            }
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("HASHER COMPLETO!");
+            btnStart.Enabled = true;
+        }
+    }
+}
